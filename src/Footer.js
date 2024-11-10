4e4f462aff7ca17f70adbba4ee999589af4bf1c3
Helen-MLT <h.milletti@gmail.com>
2024-11-10 17:20:33 +0100

    Importing code from sandbox to VSC, adding Footer component and links

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by{" "}
+      <a href="https://github.com/Helen-MLT">Hélèna Milletti</a>, is on Github
+      and hosted on Netlify
+    </p>
+  );
+}
---
+++ src/Search.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search() {
+  let [city, setCity] = useState(null);
+  let [message, setMessage] = useState(null);
+  let [weather, setWeather] = useState({});
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function displayWeather(response) {
+    setMessage(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "a5acb752426cd8188485c35694980e3a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (message) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}

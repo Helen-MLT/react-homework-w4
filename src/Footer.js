e4a8e44b6b5eb69a2d152227cef100d3be4b380b
Helen-MLT <h.milletti@gmail.com>
2024-11-10 17:23:07 +0100

    Updating links to footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,10 @@ export default function Footer() {
   return (
     <p>
       This project was coded by{" "}
-      <a href="https://github.com/Helen-MLT">Hélèna Milletti</a>, is on Github
-      and hosted on Netlify
+      <a href="https://github.com/Helen-MLT">Hélèna Milletti</a>, is on{" "}
+      <a href="https://github.com/Helen-MLT/react-homework-w4">Github</a> and
+      hosted on{" "}
+      <a href="https://super-paprenjak-edef21.netlify.app/">Netlify</a>
     </p>
   );
 }
